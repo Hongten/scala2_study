efdb0750e5c5f00514924fb5b4e872a588a78560
Hongten <hongtenzone@foxmail.com>
2019-05-18 21:40:10 +0800

    add StringDemo

---
+++ src/main/scala/com/b510/scalastudy/demo/StringDemo.scala
@@ -0,0 +1,22 @@
+package com.b510.scalastudy.demo
+
+/**
+  * 类似于java里面的String
+  * 
+  * @author Hongten
+  * @date 18/05/2019
+  */
+object StringDemo {
+
+  def main(args: Array[String]): Unit = {
+    val a = "abc"
+    val b = "abc"
+
+    println(a.equals(b)) // true
+
+    val sb = new StringBuilder
+    sb.append("a").append("b")
+    println(sb.toString()) // ab
+
+  }
+}
